00c4e74c6db6f5df5b0c38196faa452896fcb415
Prajjwal98Dubey <110735413+Prajjwal98Dubey@users.noreply.github.com>
2022-08-25 21:38:59 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import NavBar from './components/NavBar';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Home from './components/Home';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+     <NavBar/> 
+     <Routes>
+      <Route exact path='/' element={<MainPage/>}/>
+      <Route exact path='/home' element={<Home/>}/>
+      <Route  exact path='/education' element={<Education/>}/>
+      <Route  exact path='/projects' element={<Projects/>}/>
+      <Route  exact path='/skills' element={<Skills/>}/>
+     </Routes>
+    </>
   );
 }
 
